16797762e692d858c11d0d9ab73a37e9383e488f
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-10-09 20:46:24 +0300

    Add project files.

---
+++ src/Main.cpp
@@ -0,0 +1,74 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity,
+                            GLsizei length, const char* message, const void* userParam);
+
+static constexpr int initialWindowWidth  = 1280;
+static constexpr int initialWindowHeight = 960;
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
+    window = glfwCreateWindow(initialWindowWidth, initialWindowHeight, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "WARNING::GLAD: Failed to initialize OpenGL" << std::endl;
+        return -1;
+    }
+    else std::cout << "Succesfully initialized OpenGL" << std::endl;
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+
+    int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+        std::cout << "Succesfully initialized Debug Output" << std::endl;
+    }
+    else std::cout << "WARNING::DEBUG_OUTPUT: couldn't initialize" << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+
+        double xPos, yPos;
+        glfwGetCursorPos(window, &xPos, &yPos);
+
+        std::cout << "screen size: (" << width << "x" << height << ")\n";
+        std::cout << "cursor at (" << xPos << ", " << yPos << ")\n";
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
