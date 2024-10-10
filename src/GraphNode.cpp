3de53595a117b53c1bcfd787204e679ca05ddad3
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-10-10 21:31:08 +0300

    added ID to the nodes

--- src/GraphNode.cpp
+++ src/GraphNode.cpp
@@ -2,13 +2,12 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "GLFW/glfw3.h"
 
-
 GraphNode::GraphNode()
-	: m_position{ 0.0f, 0.0f }, m_color{ 1.0f, 0.0f, 0.0f }, m_size{ 30.0f, 30.0f }
+	: m_position{ 0.0f, 0.0f }, m_ID{0}, m_color{ 1.0f, 0.0f, 0.0f }, m_size{ 30.0f, 30.0f }
 {}
 
-GraphNode::GraphNode(glm::vec2 position, glm::vec2 size, glm::vec3 color)
-	: m_position{ position }, m_color{ color }, m_size{ size }
+GraphNode::GraphNode(glm::vec2 position, unsigned int ID, glm::vec2 size, glm::vec3 color)
+	: m_position{ position }, m_ID{ID}, m_color{ color }, m_size{ size }
 {}
 
 glm::vec2 GraphNode::getPosition() const
@@ -19,4 +18,9 @@ glm::vec2 GraphNode::getPosition() const
 glm::vec2 GraphNode::getSize() const
 {
 	return m_size;
+}
+
+unsigned int GraphNode::getID() const
+{
+	return m_ID;
 }
--- src/include/GraphNode.h
+++ src/include/GraphNode.h
@@ -6,12 +6,15 @@ class GraphNode
 {
 public:
 	GraphNode();
-	GraphNode(glm::vec2 position, glm::vec2 size = { 30.0f, 30.0f }, glm::vec3 color = { 1.0f, 0.0f, 0.0f });
+	GraphNode(glm::vec2 position, unsigned int ID = 0, glm::vec2 size = { 30.0f, 30.0f }, glm::vec3 color = { 1.0f, 0.0f, 0.0f });
 
 	glm::vec2 getPosition() const;
 	glm::vec2 getSize() const;
 
+	unsigned int getID() const;
+
 private:
+	unsigned int m_ID;
 	glm::vec2 m_position;
 	glm::vec2 m_size;
 	glm::vec3 m_color;
