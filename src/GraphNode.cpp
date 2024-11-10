229e5e873fc846d4b09c68bf953fcfe459852cd0
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-11-10 20:11:31 +0200

    setColor function

--- src/GraphNode.cpp
+++ src/GraphNode.cpp
@@ -2,12 +2,8 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "GLFW/glfw3.h"
 
-GraphNode::GraphNode()
-    : m_position{ 0.0f, 0.0f }, m_text{}, m_internalID{0}, m_color{ 1.0f, 0.0f, 0.0f, 1.0f}, m_size{ 30.0f, 30.0f }
-{}
-
 GraphNode::GraphNode(glm::vec2 position, const std::string& text, unsigned int ID, glm::vec2 size, glm::vec4 color)
-    : m_position{ position }, m_text{text}, m_internalID{ID}, m_color{color}, m_size{size}
+    : m_position{ position }, m_label{text}, m_internalID{ID}, m_color{color}, m_size{size}
 {}
 
 void GraphNode::setPosition(glm::vec2 position)
@@ -15,9 +11,14 @@ void GraphNode::setPosition(glm::vec2 position)
     m_position = position;
 }
 
-std::string GraphNode::getText() const
+void GraphNode::setColor(glm::vec4 color)
 {
-    return m_text;
+    m_color = color;
+}
+
+std::string GraphNode::getLabel() const
+{
+    return m_label;
 }
 
 glm::vec2 GraphNode::getPosition() const
--- src/include/GraphNode.h
+++ src/include/GraphNode.h
@@ -5,22 +5,22 @@
 class GraphNode
 {
 public:
-    GraphNode();
     GraphNode(glm::vec2 position, const std::string& text, unsigned int internalID = 0
              , glm::vec2 size = { 30.0f, 30.0f }, glm::vec4 color = { 1.0f, 0.0f, 0.0f, 1.0f});
 
     void setPosition(glm::vec2 position);
+    void setColor(glm::vec4 color);
 
-    std::string getText() const;
-    glm::vec2 getPosition() const;
-    glm::vec2 getSize() const;
-    glm::vec4 getColor() const;
+    std::string getLabel()       const;
+    glm::vec2 getPosition()      const;
+    glm::vec2 getSize()          const;
+    glm::vec4 getColor()         const;
     unsigned int getInternalID() const;
 
 private:
-    std::string m_text;
     unsigned int m_internalID;
-    glm::vec2 m_position;
-    glm::vec2 m_size;
-    glm::vec4 m_color;
+    std::string  m_label;
+    glm::vec2    m_position;
+    glm::vec2    m_size;
+    glm::vec4    m_color;
 };	
