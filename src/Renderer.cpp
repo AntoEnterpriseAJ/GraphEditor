0950daf484e8d6c7487e4eaf5db2e4137f084537
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-21 01:15:44 +0200

    edge and node batching

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -7,8 +7,10 @@
 #include "glm/gtx/rotate_vector.hpp"
 #include "ResourceManager.h"
 
+#include <thread>
+
 Renderer::Renderer()
-    : m_circleVAO{ 0 }, m_circleVBO{ 0 }, m_textRenderer{ "res/fonts/Astron.otf", 40 }
+    : m_circle{}, m_quad{}, m_textRenderer{ "res/fonts/Astron.otf", 40 }
 {
     initPrimitivesData();
 }
@@ -30,38 +32,6 @@ void Renderer::renderText(const std::string& text, const Shader& textShader, glm
     {
         m_textRenderer.RenderText(textShader, text, pos.x, pos.y, scale, color);
     }
-}
-
-void Renderer::render(GraphNode* node, Shader& nodeShader, Primitive primitive)
-{
-    nodeShader.bind();
-
-    glm::mat4 model(1.0f);
-    model = glm::translate(model, glm::vec3(node->getPosition(), 1.0f));
-    model = glm::scale(model, glm::vec3(node->getSize(), 1.0f));
-
-    int width, height;
-    glfwGetWindowSize(glfwGetCurrentContext(), &width, &height);
-    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.0f, 1.0f);
-
-    nodeShader.bind();
-    nodeShader.setMat4("model", model);
-    nodeShader.setMat4("projection", projection);
-
-    if (primitive == Primitive::circle)
-    {
-        nodeShader.setVec4("color", node->getColor());
-        glBindVertexArray(m_circleVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 90);
-    }
-    else if (primitive == Primitive::quad)
-    {
-        nodeShader.setVec4("color", node->getColor());
-        glBindVertexArray(m_quadVAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-    }
-
-    glBindVertexArray(0);
 }
 
 void Renderer::render(const Edge& edge, Shader& shader, bool oriented, bool weighted)
@@ -167,11 +137,136 @@ void Renderer::render(const Edge& edge, Shader& shader, bool oriented, bool weig
     }
 }
 
+void Renderer::render(GraphNode* node, Shader& nodeShader, PrimitiveType primitive)
+{
+    nodeShader.bind();
+
+    glm::mat4 model(1.0f);
+    model = glm::translate(model, glm::vec3(node->getPosition(), 1.0f));
+    model = glm::scale(model, glm::vec3(node->getSize(), 1.0f));
+
+    int width, height;
+    glfwGetWindowSize(glfwGetCurrentContext(), &width, &height);
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.0f, 1.0f);
+
+    nodeShader.bind();
+    nodeShader.setMat4("model", model);
+    nodeShader.setMat4("projection", projection);
+
+    if (primitive == PrimitiveType::circle)
+    {
+        nodeShader.setVec4("color", node->getColor());
+        glBindVertexArray(m_circle.VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 90);
+    }
+    else if (primitive == PrimitiveType::quad)
+    {
+        nodeShader.setVec4("color", node->getColor());
+        glBindVertexArray(m_quad.VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    }
+
+    glBindVertexArray(0);
+}
+
+void Renderer::addNodeToBatch(const GraphNode* node)
+{
+    glm::mat4 model(1.0f);
+    model = glm::translate(model, glm::vec3(node->getPosition(), 1.0f));
+    model = glm::scale(model, glm::vec3(node->getSize(), 1.0f));
+    m_circleTranslations.push_back(model); // Use move semantics only if the data is expensive to copy.
+}
+
+
+void Renderer::clearNodeBatch()
+{
+    m_circleTranslations.clear();
+}
+
+void Renderer::nodeInstanceRender(Shader& shader)
+{
+    glBindVertexArray(m_circle.VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_circleTranslationsVBO);
+    glBufferData(GL_ARRAY_BUFFER, m_circleTranslations.size() * sizeof(glm::mat4), m_circleTranslations.data(), GL_DYNAMIC_DRAW);
+
+    std::size_t vec4Size = sizeof(glm::vec4);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)0);
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(1 * vec4Size));
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(2 * vec4Size));
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(3 * vec4Size));
+
+    glVertexAttribDivisor(1, 1);
+    glVertexAttribDivisor(2, 1);
+    glVertexAttribDivisor(3, 1);
+    glVertexAttribDivisor(4, 1);
+
+    shader.bind();
+    int width, height;
+    glfwGetWindowSize(glfwGetCurrentContext(), &width, &height);
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.0f, 1.0f);
+    shader.setMat4("projection", projection);
+
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 90, static_cast<GLsizei>(m_circleTranslations.size()));
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void Renderer::addEdgeToBatch(const Edge& edge)
+{
+    glm::vec2 startPos = edge.getStartNode()->getPosition();
+    glm::vec2 endPos = edge.getEndNode()->getPosition();
+
+    m_linesData.push_back(startPos.x);
+    m_linesData.push_back(startPos.y);
+    m_linesData.push_back(endPos.x);
+    m_linesData.push_back(endPos.y);
+}
+
+
+void Renderer::clearEdgeBatch()
+{
+    m_linesData.clear();
+}
+
+void Renderer::edgeInstanceRender(Shader& shader)
+{
+    glBindVertexArray(m_line.VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_line.VBO);
+    glBufferData(GL_ARRAY_BUFFER, m_linesData.size() * sizeof(float), m_linesData.data(), GL_STATIC_DRAW);
+
+    glm::mat4 model{1.0f};
+
+    shader.bind();
+    int width, height;
+    glfwGetWindowSize(glfwGetCurrentContext(), &width, &height);
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.0f, 1.0f);
+    shader.setMat4("projection", projection);
+    shader.setMat4("model", model);
+
+    glLineWidth(0.1f);
+    glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(m_linesData.size() / 2));
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
 void Renderer::initPrimitivesData()
 {
+    glGenBuffers(1, &m_circleTranslationsVBO);
+    glGenBuffers(1, &m_linesDataVBO);
+
     initCircleData();
     initQuadData();
+    initLineData();
 }
+
 
 void Renderer::initCircleData()
 {
@@ -192,11 +287,11 @@ void Renderer::initCircleData()
         circleVertices.push_back(circleRadius * glm::sin(glm::radians(angle)));
     }
 
-    glGenVertexArrays(1, &m_circleVAO);
-    glBindVertexArray(m_circleVAO);
+    glGenVertexArrays(1, &m_circle.VAO);
+    glBindVertexArray(m_circle.VAO);
 
-    glGenBuffers(1, &m_circleVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, m_circleVBO);
+    glGenBuffers(1, &m_circle.VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_circle.VBO);
     glBufferData(GL_ARRAY_BUFFER, circleVertices.size() * sizeof(float), circleVertices.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
@@ -219,11 +314,11 @@ void Renderer::initQuadData()
         0, 1, 2, 2, 3, 0
     };
 
-    glGenVertexArrays(1, &m_quadVAO);
-    glBindVertexArray(m_quadVAO);
+    glGenVertexArrays(1, &m_quad.VAO);
+    glBindVertexArray(m_quad.VAO);
 
-    glGenBuffers(1, &m_quadVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, m_quadVBO);
+    glGenBuffers(1, &m_quad.VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_quad.VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(quadData), quadData, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
@@ -233,6 +328,26 @@ void Renderer::initQuadData()
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+}
+
+void Renderer::initLineData()
+{
+    float lineData[] = {
+        0.0f, 0.0f,
+        1.0f, 1.0f 
+    };
+
+    glGenVertexArrays(1, &m_line.VAO);
+    glBindVertexArray(m_line.VAO);
+
+    glGenBuffers(1, &m_line.VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_line.VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(lineData), lineData, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
 }
--- src/include/Renderer.h
+++ src/include/Renderer.h
@@ -7,27 +7,52 @@
 class Renderer
 {
 public:
-    enum class Primitive
+    enum class PrimitiveType
     {
         circle,
         quad,
     };
 
+    struct Primitive
+    {
+        unsigned int  VAO;
+        unsigned int  VBO;
+
+        Primitive() : VAO{0}, VBO{0} {};
+    };
+
+public:
+
     Renderer();
 
     void renderText(const std::string& text, const Shader& textShader, glm::vec2 pos, bool centered = true
                    , float scale = 1.0f, glm::vec3 color = glm::vec3{ 0.0f, 0.0f, 0.0f });
-    void render(GraphNode* node, Shader& nodeShader, Primitive primitive = Primitive::circle);
+    void render(GraphNode* node, Shader& nodeShader, PrimitiveType primitive = PrimitiveType::circle);
     void render(const Edge& edge, Shader& shader, bool oriented, bool weighted);
-    
+
+    void addNodeToBatch(const GraphNode* node);
+    void clearNodeBatch();
+    void nodeInstanceRender(Shader& shader);
+
+    void addEdgeToBatch(const Edge& edge);
+    void clearEdgeBatch();
+    void edgeInstanceRender(Shader& shader);
+
+
 private:
     void initPrimitivesData();
     void initCircleData();
     void initQuadData();
+    void initLineData();
 private:
     TextRenderer m_textRenderer;
-    unsigned int m_circleVAO;
-    unsigned int m_circleVBO;
-    unsigned int m_quadVAO;
-    unsigned int m_quadVBO;
+    Primitive    m_circle;
+    Primitive    m_quad;
+    Primitive    m_line;
+
+    unsigned int m_linesDataVBO;
+    std::vector<float> m_linesData;
+
+    unsigned int m_circleTranslationsVBO;
+    std::vector<glm::mat4> m_circleTranslations;
 };
